90f4108258ff020efc2bbcb139a6fc5d23ea36d1
Jansen <jansen.goyena@yahoo.com>
2018-07-23 15:36:03 +0800

    springboot initial commit

---
+++ src/main/java/com/becool/saveBicol/SaveBicolApplication.java
@@ -0,0 +1,12 @@
+package com.becool.saveBicol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaveBicolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaveBicolApplication.class, args);
+	}
+}
